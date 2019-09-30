eaeefa70ca4aad55434807bab325876cd4445e61
Graham Flaspoehler <grahamflas@gmail.com>
2019-09-30 17:24:04 -0400

    change db column names to match models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_30_200225) do
+ActiveRecord::Schema.define(version: 2019_09_30_212037) do
 
   create_table "items", force: :cascade do |t|
-    t.string "name"
+    t.string "item_name"
+    t.float "price"
   end
 
   create_table "listitems", force: :cascade do |t|
