1944aef38d5a2df2c880a18a3dec899167b5d663
David Fellows <dfellows12@gmail.com>
2019-10-01 16:55:27 -0400

    Added gem functionality

--- bin/run.rb
+++ bin/run.rb
@@ -4,4 +4,5 @@ require_relative '../lib/cli'
 username = get_user_name
 new_user = create_or_find_user(username)
 choose_shopping_list
+choose_option
 
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,8 @@
 ##Greet user and prompt for name##
+require "tty-prompt"
+$prompt = TTY::Prompt.new
 $current_user = nil
+$current_list = nil
 
 def get_user_name
     puts "Welcome to E-List-It! Please enter your name"
@@ -22,7 +25,7 @@ def choose_shopping_list
     if user_input == "Y"
         puts "Name your list."
         list_name = gets.chomp
-        List.create(name: list_name, user_id: $current_user.id)
+        $current_list = List.create(name: list_name, user_id: $current_user.id)
     else
         return "Thank you for trying E-List-It"
     end
@@ -31,18 +34,20 @@ end
 
 
 
-def choose_option(list)
-    puts "Choose an option"
-    puts "1. Add item 2. Remove item 3. Print list 4. Total price"
-    user_input = gets.chomp
+def choose_option
+    # puts "1. Add item 2. Remove item 3. Print list 4. Total price"
+    user_input = $prompt.select("Choose an option", ["Add item", "Remove item", "Print list", "Total price"])
     if user_input == "Add item"
-        ListItem.create(item)
+        item_names = Item.all.map {|item| item.item_name}
+        new_items = $prompt.multi_select("Please select items to add to list.", item_names)
+        binding.pry
+
     elsif user_input == "Remove item"
 
     elsif user_input == "Print list"
-        list.items.map {|item| puts item.name}
+        $current_list.items.map {|item| puts item.name}
     elsif user_input == "Total price"
-        list.total_price
+        $current_list.total_price
     else
         puts "That is not an opton, please choose again"
         choose_option
