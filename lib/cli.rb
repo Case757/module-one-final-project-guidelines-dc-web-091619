23c092db3a1857a8f4ec7caef4ff4c7e42b4ec41
Graham Flaspoehler <grahamflas@gmail.com>
2019-10-02 11:56:21 -0400

    need to fix bug in 'Remove Item'

--- lib/cli.rb
+++ lib/cli.rb
@@ -37,7 +37,7 @@ end
 
 def choose_option
     # puts "1. Add item 2. Remove item 3. Print list 4. Total price"
-    user_input = $prompt.select("Choose an option", ["Add item", "Remove item", "Print list", "Total price"])
+    user_input = $prompt.select("Choose an option", ["Add item", "Remove item", "Print list", "Total price", "Exit E-List-It"])
 
     if user_input == "Add item"
         
@@ -60,28 +60,45 @@ def choose_option
         test_array = ListItem.all.map {|li| li.list_id}
         
         if test_array.include?($current_list.id)
-            $prompt.multi_select("Please select items to remove from list.", $current_list.item_name)
+            remove_items = $prompt.multi_select("Please select items to remove from list.", $current_list.items.map {|item| item.item_name})
+
+            remove_items.each do |item|
+                this_item = Item.find_by(item_name: item)
+                
+                ListItem.destroy_by(item_id: this_item.id, list_id: $current_list.id)
+            end
+
+            puts "Your list has been updated"
+            choose_option
+
+            
+
         else
             puts "There are no items on your list, please create a list."
             choose_option            
         end
 
     elsif user_input == "Print list"
+        binding.pry
         $current_list.items.map do |item| 
             puts item.item_name
         end
+        choose_option
 
     elsif user_input == "Total price"
-        $current_list.total_price
-    else
-        puts "That is not an opton, please choose again"
+        puts "The items in your list total to $#{$current_list.total_price}."
         choose_option
+
+    elsif user_input == "Exit E-List-It"
+        puts "********************************"
+        puts "See ya."
+        puts "********************************"
     end
 end
 
 def print_items_added(array)
     puts "These items have been added to your shopping list:"
-    array.each do |item|
+    array.each do |item|110
         puts item
     end
 end
