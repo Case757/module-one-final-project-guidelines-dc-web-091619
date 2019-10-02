a821dbe8c354ba64d964798f7f439fb4a128d8b6
David Fellows <dfellows12@gmail.com>
2019-10-02 15:39:54 -0400

    Redo cli

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,12 @@
 require_relative '../config/environment'
 require_relative '../lib/cli'
 
-username = Cli.get_user_name
-new_user = Cli.create_or_find_user(username)
-Cli.choose_shopping_list
-Cli.choose_option
+list = Cli.new
+list.runner
+
+
+# username = Cli.get_user_name
+# new_user = Cli.create_or_find_user(username)
+# Cli.choose_shopping_list
+# Cli.choose_option
 
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,36 +1,48 @@
 ##Greet user and prompt for name##
 require "tty-prompt"
 class Cli
+    attr_accessor :current_user, :current_list, :prompt
     @@prompt = TTY::Prompt.new
-    @@current_user = nil
-    @@current_list = nil
 
-    def self.current_user
-        @@current_user
+    def runner
+        user_name = get_user_name
+        new_user = create_or_find_user(user_name)
+        choose_shopping_list
+        user_input = choose_option
+        choice_menu(user_input)
+        
     end
 
-    def self.current_user=(a)
-        @@current_user = a
+    def choice_menu(user_input)
+        
+        case user_input
+        when "Add item" 
+            add_item(user_input)
+            user_input = choose_option
+            choice_menu(user_input)
+        when "Remove item"
+            remove_item(user_input)
+            user_input = choose_option
+            choice_menu(user_input)
+        when "Print list"
+            print_list(user_input)
+            user_input = choose_option
+            choice_menu(user_input)
+        when "Total price"
+            total_price(user_input)
+            user_input = choose_option
+            choice_menu(user_input)
+        when "Exit E-List-It"
+            exit_list(user_input)
+        end  
     end
 
-    def self.current_list
-        @@current_list
-    end
-
-    def self.current_list=(a)
-        @@current_list = a
-    end
-
-    def self.prompt
-        @@prompt
-    end
-
-    def self.get_user_name
+    def get_user_name
         puts "Welcome to E-List-It! Please enter your name"
         user_name = gets.chomp
     end
 
-    def self.create_or_find_user(user_name)
+    def create_or_find_user(user_name)
         if User.names.include?(user_name)
             # puts "Would you like to use a previous list? Y/N"
             # user_input = gets.chomp
@@ -39,7 +51,7 @@ class Cli
         end
     end
 
-    def self.choose_shopping_list
+    def choose_shopping_list
         ##refactor to use prompt gem instead of getting user input
         puts "Would you like to create a new shopping list? Y/N"
         user_input = gets.chomp
@@ -54,34 +66,32 @@ class Cli
         end
     end
 
+    def choose_option
+        user_input = @@prompt.select("Choose an option", ["Add item", "Remove item", "Print list", "Total price", "Exit E-List-It"])
+    end
 
-
-    def self.choose_option
-        # puts "1. Add item 2. Remove item 3. Print list 4. Total price"
-        user_input = prompt.select("Choose an option", ["Add item", "Remove item", "Print list", "Total price", "Exit E-List-It"])
-
-        if user_input == "Add item"
+    def add_item(user_input)
+        if  user_input == "Add item"
             
             item_names = Item.all.map {|item| item.item_name}
-            new_items = prompt.multi_select("Please select items to add to list.", item_names)
+            new_items = @@prompt.multi_select("Please select items to add to list.", item_names)
 
-            
+    
             new_items.each do |item|
                 this_item = Item.find_by(item_name: item)
-                
                 ListItem.create(item_id: this_item.id, list_id: self.current_list.id)
             end
+        end
+        puts "Your list has been updated"
+    end
 
-            puts "Your list has been updated"
-            choose_option
-
-
-        elsif user_input == "Remove item"
+    def remove_item(user_input)
+        if user_input == "Remove item"
             
             test_array = ListItem.all.map {|li| li.list_id}
             
             if test_array.include?(self.current_list.id)
-                remove_items = prompt.multi_select("Please select items to remove from list.", self.current_list.items.map {|item| item.item_name})
+                remove_items = @@prompt.multi_select("Please select items to remove from list.", self.current_list.items.map {|item| item.item_name})
 
                 remove_items.each do |item|
                     this_item = Item.find_by(item_name: item)
@@ -91,36 +101,39 @@ class Cli
 
                 puts "Your list has been updated"
                 self.current_list.reload
-                choose_option
-
-                
 
             else
-                puts "There are no items on your list, please create a list."
-                choose_option            
+                puts "There are no items on your list, please create a list."          
             end
+        end
+    end
 
-        elsif user_input == "Print list"
+    def print_list(user_input)
+        if user_input == "Print list"
             
             self.current_list.items.map do |item| 
                 puts item.item_name
             end
-            choose_option
+        end
+    end
 
-        elsif user_input == "Total price"
+    def total_price(user_input)
+        if user_input == "Total price"
             puts "The items in your list total to $#{self.current_list.total_price}."
-            choose_option
+        end
+    end
 
-        elsif user_input == "Exit E-List-It"
+    def exit_list(user_input)
+        if user_input == "Exit E-List-It"
             puts "********************************"
             puts "See ya."
             puts "********************************"
         end
     end
 
-    def self.print_items_added(array)
+    def print_items_added(array)
         puts "These items have been added to your shopping list:"
-        array.each do |item|110
+        array.each do |item|
             puts item
         end
     end
